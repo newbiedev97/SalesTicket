48256fba933a57d3c74bde0876d7ac3768077ae0
AhmadR3zA <46918547+AhmadR3zA1911@users.noreply.github.com>
2019-06-19 00:03:02 +0430

    create user and role firts run
    add method to starup file for create user and role in first run :D
    
    Co-Authored-By: solmazjavanirad <solmazjavanirad@users.noreply.github.com>
    Co-Authored-By: VAHID SHAPOURI <vshapouri@users.noreply.github.com>

--- SaleTicket/Startup.cs
+++ SaleTicket/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using SaleTicket.Models;
 
 [assembly: OwinStartupAttribute(typeof(SaleTicket.Startup))]
 namespace SaleTicket
@@ -9,6 +12,32 @@ namespace SaleTicket
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRoleAndUser();
+        }
+
+        public void CreateRoleAndUser()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            var userMgr = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var roleMgr = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+
+            if (!roleMgr.RoleExists("Admin"))
+            {
+                var role = new IdentityRole("Admin");
+                roleMgr.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "Admin1";
+                user.Email = "Admin1@Gmail.com";
+                string pw = "Admin@123";
+                var newUSer = userMgr.Create(user, pw);
+                if(newUSer.Succeeded)
+                {
+                    var result = userMgr.AddToRole(user.Id, "Admin");
+                }
+
+            }
+
         }
     }
 }
